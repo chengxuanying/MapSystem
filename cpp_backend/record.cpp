d7329f151482da416deaba10c12d3f31ea3fb34a
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-11 15:00:15 +0800

    初始化cpp代码

---
+++ cpp_backend/record.cpp
@@ -0,0 +1,47 @@
+//
+// Created by 派星星 on 2019/11/11.
+//
+#include "record.h"
+#include "utils.h"
+
+void endianChange(struct _struct_record &t) {
+    // change the endian mode
+    t._struct_size = swap_uint16(t._struct_size);
+    t._id = swap_uint32(t._id);
+    t._name_size = swap_uint16(t._name_size);
+    t._info = swap_uint32(t._info);
+}
+
+int readDat() {
+    // create tmp struct
+    struct _struct_record _tmp_struct_record;
+    char _tmp_string[100];
+    int _string_size = 0;
+
+    FILE *fptr = fopen("../GTBL.dat", "rb");
+
+    // if not have file than raise error
+    if (fptr == NULL) {
+        printf("Cannot find GTBL.dat!");
+        exit(1);
+    }
+
+    cout << "struct_size:" << sizeof(_struct_record) << endl;
+    int cnt = 0;
+    while (fread(&_tmp_struct_record, sizeof(_struct_record), 1, fptr) == 1) {
+
+        endianChange(_tmp_struct_record);
+        _string_size = _tmp_struct_record._struct_size - 12; // 12 = 2 + 4 + 2 + 4
+        fread(_tmp_string, _string_size * sizeof(char), 1, fptr);
+        _tmp_string[_string_size + 1] = 0;
+
+
+        cout << _tmp_string << ';' << endl;
+        char *a = "123是23";
+        cout << a << endl;
+        cout << ++cnt << ":" << _tmp_struct_record._name_size << endl;
+    }
+
+
+    fclose(fptr);
+}
---
+++ cpp_backend/utils.h
@@ -0,0 +1,18 @@
+//
+// Created by 派星星 on 2019/11/11.
+//
+
+#ifndef CPP_BACKEND_UTILS_H
+#define CPP_BACKEND_UTILS_H
+
+#endif //CPP_BACKEND_UTILS_H
+
+#include <stdint.h>
+
+uint16_t swap_uint16(uint16_t val);
+
+int16_t swap_int16(int16_t val);
+
+uint32_t swap_uint32(uint32_t val);
+
+int32_t swap_int32(int32_t val);
