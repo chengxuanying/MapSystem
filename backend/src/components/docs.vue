e94e94a24c9ed4fd2a4e72a9c90e51cedaf8fe44
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-12 15:30:45 +0800

    增加项目文档

--- backend/src/components/docs.vue
+++ backend/src/components/docs.vue
@@ -1,18 +1,31 @@
 <template>
 
-    <el-tabs tab-position="left" style="height: 200px;">
+    <el-tabs tab-position="left">
         <el-tab-pane label="系统架构">
             <div style="margin-left: 30px;">
                 <h2>系统架构</h2>
 
                 <h3>系统架构图</h3>
-                <p>你好</p>
-<!--                <div class="block">-->
-<!--                    <span class="demonstration">默认</span>-->
-                    <el-image src="dist/docs1.png" fit="contain"
-                    style="width: 100px; height: 100px"></el-image>
-<!--                </div>-->
-                <p>dsa</p>
+                <p>下图是我们的系统架构图。我们的系统架构主要分为前端ui展示层，业务层，计算层，数据库，运行环境六部分。</p>
+                <el-image src="dist/docs1.png" fit="contain" style="width: 80%;"></el-image>
+
+
+                <p>其中，我们的前端又挨包括element ui，包括CSS，包括JavaScript以及我们美工，黄俊文童鞋，制作的图片。
+                </p>
+                <p>我们的展示层包括VUE引擎，包括Ajax，读取的是Restful API，也包括我们的Get请求。
+                </p>
+                <p>我们的业务层包括首页数据分析，数据检索，数据更新，排序比较和说明文档等内容。
+                </p>
+                <p>我们的计算层包括读写过程，数据缓存排序过程，自定义排序，JSON格式化等内容。
+                </p>
+                <p>我们的数据库读写老师给定的data格式二进制文件。
+                </p>
+                <p>我们的运行环境为Linux，当然同样可以运行在虚拟机环境中。
+                </p>
+                <p>我们的业务层和计算层之间实现了负载均衡。
+                </p>
+                <p>我们的展示层，业务层、计算层，对外提供端对端的服务。</p>
+
 
             </div>
 
@@ -20,14 +33,14 @@
         </el-tab-pane>
 
         <el-tab-pane label="前端">
-            <div>
+            <div style="margin-left: 30px;">
                 前端
             </div>
 
         </el-tab-pane>
 
         <el-tab-pane label="后端">
-            <div>
+            <div style="margin-left: 30px;">
                 后端
             </div>
 
@@ -35,15 +48,19 @@
         </el-tab-pane>
 
         <el-tab-pane label="计算端">
-            <div>
-                计算端
+            <div style="margin-left: 30px;">
+                <h2>计算端</h2>
+
+                <h3>类图</h3>
+
+                <h3>算法流程图</h3>
+
+                <h3>关键数据结构</h3>
+
+                <h3>关键接口</h3>
             </div>
 
         </el-tab-pane>
     </el-tabs>
 
 </template>
-
-<script>
-
-</script>
--- backend/src/components/home.vue
+++ backend/src/components/home.vue
@@ -5,13 +5,13 @@
       <!--el-carousel-item v-for="item in photos" :key="item">
           <h3 class="small">{{ item }}</h3>
       </el-carousel-item-->
-      <img src="dist/2.png" class="carousel_image_type" fit="fill">
+      <el-image src="dist/2.png" class="carousel_image_type" fit="contain"></el-image>
     </el-carousel-item>
     <el-carousel-item>
       <!--el-carousel-item v-for="item in photos" :key="item">
           <h3 class="small">{{ item }}</h3>
       </el-carousel-item-->
-      <img src="dist/1.png" class="carousel_image_type" fit="fill">
+      <el-image src="dist/1.png" class="carousel_image_type" fit="fill"></el-image>
     </el-carousel-item>
   </el-carousel>
 
