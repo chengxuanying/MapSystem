4cd7c9d8efad001402ba9c861a1788a111ebf1f8
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-12 09:14:14 +0800

    修改首页

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -1,190 +1,200 @@
 <template>    <!--模-->
-  <div id="app">
-
-<el-container>
-  <el-header>
-<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">E-map</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">关于我们</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu>
-  </el-submenu>
-  <el-menu-item index="3" >排序</el-menu-item>
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">更新信息</a></el-menu-item>
-  <el-menu-item index="5" >数据分析</el-menu-item>
-</el-menu>
-<div class="line"></div>
-</el-header>
+    <div id="app">
 
 
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-container>
-      <el-main>
-  <el-carousel indicator-position="outside">
-    <el-carousel-item v-for="item in 4" :key="item">
-      <h3>{{ item }}</h3>
-    </el-carousel-item>
-  </el-carousel>
-
-</el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
-  </el-container>
-</el-container>
-
-<el-container>
-  <el-header>
-<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu>
-  </el-submenu>
-  <el-menu-item index="3" >消息中心</el-menu-item>
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-</el-menu>
-<div class="line"></div>
-</el-header>
+        <!--        <el-container>-->
 
 
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-container>
-      <el-main>
- 
-</el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
-  </el-container>
-</el-container>
-
-<el-container>
-  <el-header>
-<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu>
-  </el-submenu>
-  <el-menu-item index="3" >消息中心</el-menu-item>
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-</el-menu>
-<div class="line"></div>
-</el-header>
+        <!--                        <div class="grid-content bg-purple"></div>-->
+        <el-row :gutter="20">
+            <el-col :span="4">
+                <div class="grid-content bg-purple"></div>
+            </el-col>
+            <el-col :span="16">
+                <el-container>
+                    <el-header>
+                        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal"
+                                 @select="handleSelect">
+                            <el-menu-item index="1"><i class="el-icon-place"></i>Mapsystem</el-menu-item>
+                            <el-menu-item index="3">数据分析</el-menu-item>
+                            <el-menu-item index="4">排序算法分析</el-menu-item>
+                            <el-menu-item index="5">项目文档</el-menu-item>
+                        </el-menu>
+                    </el-header>
 
 
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-container>
-      <el-main>
-
-</el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
-  </el-container>
-</el-container>
+                    <el-carousel height="190px">
+                        <el-carousel-item v-for="item in 4" :key="item">
+                            <h3 class="small">{{ item }}</h3>
+                        </el-carousel-item>
+                    </el-carousel>
 
 
-  </div>
+                    <el-footer>
+                        <el-divider></el-divider>
+                        <el-alert
+                                title="Made with love by 3 boys."
+                                type="info"
+                                close-text=" ">
+                        </el-alert>
+                   </el-footer>
+
+                </el-container>
+            </el-col>
+            <el-col :span="4">
+                <div class="grid-content bg-purple"></div>
+            </el-col>
+
+
+        </el-row>
+
+
+        <!--        </el-container>-->
+
+        <!--        <el-container>-->
+        <!--            <el-header>-->
+        <!--                <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">-->
+        <!--                    <el-menu-item index="1">处理中心</el-menu-item>-->
+        <!--                    <el-submenu index="2">-->
+        <!--                        <template slot="title">我的工作台</template>-->
+        <!--                        <el-menu-item index="2-1">选项1</el-menu-item>-->
+        <!--                        <el-menu-item index="2-2">选项2</el-menu-item>-->
+        <!--                        <el-menu-item index="2-3">选项3</el-menu-item>-->
+        <!--                        <el-submenu index="2-4">-->
+        <!--                            <template slot="title">选项4</template>-->
+        <!--                            <el-menu-item index="2-4-1">选项1</el-menu-item>-->
+        <!--                            <el-menu-item index="2-4-2">选项2</el-menu-item>-->
+        <!--                            <el-menu-item index="2-4-3">选项3</el-menu-item>-->
+        <!--                        </el-submenu>-->
+        <!--                    </el-submenu>-->
+        <!--                    <el-menu-item index="3">消息中心</el-menu-item>-->
+        <!--                    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>-->
+        <!--                </el-menu>-->
+        <!--                <div class="line"></div>-->
+        <!--            </el-header>-->
+
+        <!--        </el-container>-->
+
+        <!--        <el-container>-->
+        <!--            <el-header>-->
+        <!--                <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">-->
+        <!--                    <el-menu-item index="1">处理中心</el-menu-item>-->
+        <!--                    <el-submenu index="2">-->
+        <!--                        <template slot="title">我的工作台</template>-->
+        <!--                        <el-menu-item index="2-1">选项1</el-menu-item>-->
+        <!--                        <el-menu-item index="2-2">选项2</el-menu-item>-->
+        <!--                        <el-menu-item index="2-3">选项3</el-menu-item>-->
+        <!--                        <el-submenu index="2-4">-->
+        <!--                            <template slot="title">选项4</template>-->
+        <!--                            <el-menu-item index="2-4-1">选项1</el-menu-item>-->
+        <!--                            <el-menu-item index="2-4-2">选项2</el-menu-item>-->
+        <!--                            <el-menu-item index="2-4-3">选项3</el-menu-item>-->
+        <!--                        </el-submenu>-->
+        <!--                    </el-submenu>-->
+        <!--                    <el-menu-item index="3">消息中心</el-menu-item>-->
+        <!--                    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>-->
+        <!--                </el-menu>-->
+        <!--                <div class="line"></div>-->
+        <!--            </el-header>-->
+
+
+        <!--        </el-container>-->
+
+
+    </div>
 
 </template>
 
 <script>
-export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
+    export default {
+        methods: {
+            startHacking() {
+                this.$notify({
+                    title: 'It works!',
+                    type: 'success',
+                    message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+                    duration: 5000
+                })
+            }
+        }
     }
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-     }
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-  
-  body > .el-container {
-    margin-bottom: 80px;
-  }
-    .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-  
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 150px;
-    margin: 0;
-  }
+    #app {
+        font-family: Helvetica, sans-serif;
+        text-align: center;
+    }
 
-  .el-carousel__item:nth-child(2n) {
-     background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-     background-color: #d3dce6;
-  }
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+    }
+
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+    }
+
+    body > .el-container {
+        margin-bottom: 80px;
+    }
+
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 18px;
+        opacity: 0.75;
+        line-height: 300px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 14px;
+        opacity: 0.75;
+        line-height: 150px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+
+    .el-col {
+        border-radius: 4px;
+    }
+
+
+    .bg-purple {
+        /*background: #d3dce6;*/
+    }
+
+
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+
+    .row-bg {
+        padding: 10px 0;
+        background-color: #f9fafc;
+    }
 </style>
 
