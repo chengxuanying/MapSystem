b67f7e354b8bb39b1b1f7639f3180c68591b3d3b
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-12 19:09:51 +0800

    修改算法分析界面样式

--- backend/app.py
+++ backend/app.py
@@ -6,14 +6,15 @@ app = Flask(__name__,
             static_url_path="",
             static_folder="dist")
 
+
 # 跨域支持
 def after_request(resp):
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
+
 app.after_request(after_request)
 database = None
-
 
 
 @app.route('/retrieve/test', methods=['GET'])
@@ -31,9 +32,10 @@ def get_tot():
 
 @app.route('/retrieve/page_size/<int:page_size>/page/<int:page>', methods=['GET'])
 def get_page(page_size, page):
-    start = (page - 1 ) * page_size
+    start = (page - 1) * page_size
     end = page * page_size
     return json.dumps({"result": database['result'][start: end], "tot": database['cnt']})
+
 
 @app.route('/retrieve/sort/<string:column>/<string:order>', methods=['GET'])
 def sort_page(column, order):
@@ -56,7 +58,6 @@ def sort_page(column, order):
     return json.dumps({"result": None})
 
 
-
 @app.route('/retrieve/<string:arg1>/<string:arg2>', methods=['GET'])
 def retrieve_something(arg1, arg2):
     r = os.popen('./test/cpp_backend retrieve {} {}'.format(arg1, arg2))
@@ -65,18 +66,19 @@ def retrieve_something(arg1, arg2):
 
     return text
 
+
 @app.route('/sort', methods=['GET'])
 def sort():
-    #r = os.popen('./test/cpp_backend sort'.format(arg1, arg2))
-    #text = r.read()
-    #r.close()
+    # r = os.popen('./test/cpp_backend sort'.format(arg1, arg2))
+    # text = r.read()
+    # r.close()
     text = [
-    { '数据量': '10', '访问用户': 1393, '下单用户': 1093},
-    { '数据量': '100', '访问用户': 3530, '下单用户': 3230},
-    { '数据量': '1000', '访问用户': 2923, '下单用户': 2623},
-    { '数据量': '10000', '访问用户': 1723, '下单用户': 1423},
-    { '数据量': '20000', '访问用户': 3792, '下单用户': 3492},
-    { '数据量': '50000', '访问用户': 4593, '下单用户': 4293}
+        {'数据量': '10', '冒泡排序': 1393, '插入排序': 1093},
+        {'数据量': '100', '冒泡排序': 3530, '插入排序': 3230},
+        {'数据量': '1000', '冒泡排序': 2923, '插入排序': 2623},
+        {'数据量': '10000', '冒泡排序': 1723, '插入排序': 1423},
+        {'数据量': '20000', '冒泡排序': 3792, '插入排序': 3492},
+        {'数据量': '50000', '冒泡排序': 4593, '插入排序': 4293}
     ]
     return json.dumps({"result": text})
 
--- backend/src/components/sort.vue
+++ backend/src/components/sort.vue
@@ -2,104 +2,105 @@
     <el-container direction='vertical'>
         <el-container>
             <el-main>
-                <h2>这里是标题： </h2>
-                <h3>这里是描述1： </h3>
+                <h2> 算法对比 </h2>
+                <div class="margin_down">下图展示了数量N为自变量，时间T为因变量的算法执行时间图，探究N与T的关系：</div>
+                <el-steps :space="150" :active="0" finish-status="success">
+                    <el-step title="等待中"></el-step>
+                    <el-step title="快速排序"></el-step>
+                    <el-step title="桶排序"></el-step>
+                    <el-step title="堆排序"></el-step>
+                    <el-step title="插入排序"></el-step>
+                    <el-step title="冒泡排序"></el-step>
+                </el-steps>
+
             </el-main>
-            <el-containter>
-                <ve-line :data="chartData" :settings="chartSettings"></ve-line>
-            </el-containter>
+
+            <el-row>
+                <el-col :span="3">
+                    &nbsp;
+                </el-col>
+
+                <el-col :span="16">
+                    <el-containter>
+                        <ve-line :data="chartData" :settings="chartSettings"></ve-line>
+                    </el-containter>
+                </el-col>
+
+                <el-col :span="4">
+                    &nbsp;
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col :span="3">
+                    &nbsp;
+                </el-col>
+
+                <el-col :span="16">
+                    <el-col :span="11">
+                        <el-button type="primary" icon="el-icon-switch-button" class="margin_down">运行全部</el-button>
+                    </el-col>
+                </el-col>
+
+                <el-col :span="4">
+                    &nbsp;
+                </el-col>
+            </el-row>
+
+
             <el-footer>
-                <h3>这里是描述2: </h3>
+                <div class="margin_down">从如上结果中我们不难看出。。。。</div>
             </el-footer>
         </el-container>
-        <el-container>
-            <el-main>
-                <el-divider></el-divider>
-                <h2>这里是标题： </h2>
-                <h3>这里是描述1： </h3>
-            </el-main>
-            <el-containter>
-                <ve-line :data="chartData" :settings="chartSettings"></ve-line>
-            </el-containter>
-            <el-footer>
-                <h3>这里是描述2: </h3>
-            </el-footer>
-        </el-container>
-        <el-container>
-            <el-main>
-                <el-divider></el-divider>
-                <h2>这里是标题： </h2>
-                <h3>这里是描述1： </h3>
-            </el-main>
-            <el-containter>
-                <ve-line :data="chartData" :settings="chartSettings"></ve-line>
-            </el-containter>
-            <el-footer>
-                <h3>这里是描述2: </h3>
-            </el-footer>
-        </el-container>
-        <el-container>
-            <el-main>
-                <el-divider></el-divider>
-                <h2>这里是标题： </h2>
-                <h3>这里是描述1： </h3>
-            </el-main>
-            <el-containter>
-                <ve-line :data="chartData" :settings="chartSettings"></ve-line>
-            </el-containter>
-            <el-footer>
-                <h3>这里是描述2: </h3>
-            </el-footer>
-        </el-container>
-        <el-container>
-            <el-main>
-                <el-divider></el-divider>
-                <h2>这里是标题： </h2>
-                <h3>这里是描述1： </h3>
-            </el-main>
-            <el-containter>
-                <ve-line :data="chartData" :settings="chartSettings"></ve-line>
-            </el-containter>
-            <el-footer>
-                <h3>这里是描述2: </h3>
-            </el-footer>
-        </el-container>
+
+        
     </el-container>
-  
+
 </template>
 
 <script>
 
-import axios from 'axios'
-export default {
-    data () {
-        this.chartSettings = {
-        metrics: ['访问用户', '下单用户'],
-        dimension: ['数据量']
-        }
-        return {
-        chartData: {
-            columns: ['数据量', '访问用户', '下单用户'],
-            rows: []
-        }
-        };
-    },
-    created(){
-    var self = this;
-        var url = `http://127.0.0.1:5000/sort`;
+    import axios from 'axios'
 
-        axios.get(url).then(function (res) {
-            self.chartData.rows = res.data.result;
-            //console.log(res.data);
-            //console.log(self.chartData.rows);
-        }).catch(function (error) {
-            console.log(error);
-        });
+    export default {
+        data() {
+            this.chartSettings = {
+                metrics: ['冒泡排序', '插入排序'],
+                yAxisName: ['T/ms'],
+                xAxisName: ['N/记录数'],
+                dimension: ['数据量']
+            }
+            return {
+                chartData: {
+                    columns: ['数据量', '访问用户', '下单用户'],
+                    rows: []
+                }
+            };
+        },
+        created() {
+            var self = this;
+            var url = `http://127.0.0.1:5000/sort`;
+
+            axios.get(url).then(function (res) {
+                self.chartData.rows = res.data.result;
+                //console.log(res.data);
+                //console.log(self.chartData.rows);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
     }
-}
 </script>
 <style>
-.el-footer{
-    padding:22px;
-}
+    .el-footer {
+        padding: 22px;
+    }
+
+    .margin_down {
+        margin-bottom: 15px;
+    }
+
+    .margin_right {
+        margin-right: 15px;
+    }
 </style>
