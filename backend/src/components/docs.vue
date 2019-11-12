58b91581abb77b68e09209f69008cbf41a8ca7e8
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-12 14:57:00 +0800

    增加docs

---
+++ backend/src/components/docs.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <el-tabs tab-position="left" style="height: 200px;">
+        <el-tab-pane label="系统架构">
+            <div style="margin-left: 30px;">
+                <h2>系统架构</h2>
+
+                <h3>系统架构图</h3>
+                <p>你好</p>
+<!--                <div class="block">-->
+<!--                    <span class="demonstration">默认</span>-->
+                    <el-image src="dist/docs1.png" fit="contain"
+                    style="width: 100px; height: 100px"></el-image>
+<!--                </div>-->
+                <p>dsa</p>
+
+            </div>
+
+
+        </el-tab-pane>
+
+        <el-tab-pane label="前端">
+            <div>
+                前端
+            </div>
+
+        </el-tab-pane>
+
+        <el-tab-pane label="后端">
+            <div>
+                后端
+            </div>
+
+
+        </el-tab-pane>
+
+        <el-tab-pane label="计算端">
+            <div>
+                计算端
+            </div>
+
+        </el-tab-pane>
+    </el-tabs>
+
+</template>
+
+<script>
+
+</script>
