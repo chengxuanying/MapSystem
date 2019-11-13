427d392fc698cff0a450344c9ef347276b3756b8
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-13 22:13:55 +0800

    test

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,11 @@ from flask import Flask, stream_with_context, Response, render_template
 import os
 import json
 import time
+import locale
+locale.setlocale(locale.LC_ALL,'zh_CN.UTF-8')
 
-app_dir = "/mapsystem/backend/cpp_backend"
+# app_dir = "/mapsystem/backend/cpp_backend"
+app_dir = "../cpp_backend/my_cpp_backend"
 
 app = Flask(__name__,
             static_url_path="",
