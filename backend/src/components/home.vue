bf0d4be47e6b70496647efec49bbc6a337aa9cfc
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-14 08:56:40 +0800

    修改首页缩放比例

--- backend/src/components/home.vue
+++ backend/src/components/home.vue
@@ -1,7 +1,7 @@
 <template>
 
     <el-container direction="vertical">
-        <el-carousel  class="margin_down2">
+        <el-carousel class="margin_down2">
             <el-carousel-item>
                 <!--el-carousel-item v-for="item in photos" :key="item">
                     <h3 class="small">{{ item }}</h3>
@@ -14,7 +14,7 @@
                 </el-carousel-item-->
                 <el-image src="../assets/16.jpg" class="carousel_image_type" fit="contain"></el-image>
             </el-carousel-item>
- <el-carousel-item>
+            <el-carousel-item>
                 <!--el-carousel-item v-for="item in photos" :key="item">
                     <h3 class="small">{{ item }}</h3>
                 </el-carousel-item-->
@@ -24,7 +24,7 @@
 
         <el-row type="flex" justify="space-around">
             <el-col :span="6">
-                <el-card :body-style="{ padding: '5px' }" >
+                <el-card :body-style="{ padding: '5px' }">
                     <img src="assets/8.png"
                          class="image">
                     <div style="padding: 18px; text-align: center;">
@@ -34,7 +34,7 @@
             </el-col>
             <el-col :span="6">
                 <el-card :body-style="{ padding: '1px' }">
-                   <img src="assets/9.png"
+                    <img src="assets/9.png"
                          class="image">
                     <div style="padding: 22px; text-align: center;">
                         <span>可视化设计</span>
@@ -42,8 +42,8 @@
                 </el-card>
             </el-col>
             <el-col :span="6">
-                <el-card :body-style="{ padding: '2px' }" >
-                   <img src="assets/10.png"
+                <el-card :body-style="{ padding: '2px' }">
+                    <img src="assets/10.png"
                          class="image">
                     <div style="padding: 20px; text-align: center;">
                         <span>高可用服务</span>
@@ -62,5 +62,12 @@
 <style>
     .margin_down2 {
         margin-bottom: 30px;
+    }
+
+    .image {
+        width: auto;
+        height: auto;
+        max-width: 100%;
+        max-height: 100%;
     }
 </style>
